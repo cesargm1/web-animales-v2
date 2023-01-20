0245a447eb3df5fee3ba2b1665c2556815d99cca
Cesar <cg.gtav@gmail.com>
2023-01-20 20:35:27 +0100

    añadi boton ver mas

--- javascript/index.js
+++ javascript/index.js
@@ -8,3 +8,14 @@ function closeMenu() {
   document.getElementById('submenu_type_animals').classList.add('hidden')
 }
 
+function toggleText(numero){
+  document.getElementById('more-info-' + numero).classList.toggle('hidden')
+
+  const button = document.getElementById('hiden-text-' + numero)
+
+  if (button.innerText == 'ver menos') {
+    button.innerText = 'ver mas'
+  } else {
+    button.innerText = 'ver menos'
+  }
+}
